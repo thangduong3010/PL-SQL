d37846884279186bb1a419ab51653dadfaddeceb
Thang <thangduong.hanu@gmail.com>
2016-01-08 16:26:44 +0700

    dot

---
+++ Practice/Package_deptpkg_body.sql
@@ -0,0 +1,24 @@
+/* Example of overloading */
+
+CREATE OR REPLACE PACKAGE BODY dept_pkg
+IS
+   PROCEDURE add_department (
+      p_deptno    departments.department_id%TYPE,
+      p_name      departments.department_name%TYPE := 'Unknown',
+      p_loc       departments.location_id%TYPE := 1700)
+   IS
+   BEGIN
+      INSERT INTO departments (department_id, department_name, location_id)
+           VALUES (p_deptno, p_name, p_loc);
+   END add_department;
+
+   PROCEDURE add_department (
+      p_name    departments.department_name%TYPE := 'Unknown',
+      p_loc     departments.location_id%TYPE := 1700)
+   IS
+   BEGIN
+      INSERT INTO departments (department_id, department_name, location_id)
+           VALUES (departments_seq.NEXTVAL, p_name, p_loc);
+   END add_department;
+END dept_pkg;
+/
